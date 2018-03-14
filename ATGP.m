e873bd177298a98c53b842b57c90186228ba016b
alantwm <alantwm@gmail.com>
2018-03-14 11:11:55 +0800

    Updated ATGP description

--- ATGP.m
+++ ATGP.m
@@ -1,6 +1,12 @@
 classdef ATGP
 % ATGP is a gaussian process-based supervised transfer learning algorithm 
-% based on a paper by Cao et. al.
+% based on a paper by Cao et. al. This implementation utilizes the squared 
+% exponential kernel function and the associated hyperparameters are optimized 
+% using global search algorithm (CMAES).
+%
+% Described in detail in:
+% Cao, Bin, Sinno Jialin Pan, Yu Zhang, Dit-Yan Yeung, and Qiang Yang. "Adaptive 
+% Transfer Learning." In AAAI, vol. 2, no. 5, p. 7. 2010.
 % 
 % ATGP takes as inputs (x_target,y_target,x_source,y_source)
 % Accepted inputs are shaped (n,d); n = # of instances, d = dimensions
